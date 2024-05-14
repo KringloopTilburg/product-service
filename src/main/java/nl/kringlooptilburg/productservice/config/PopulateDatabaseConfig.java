81f700ac9192f3caedaabaa40e2a4d761f3196fc
i476933@fhict.nl <476933@student.fontys.nl>
2024-05-14 11:18:08 +0200

    its so joever

--- src/main/java/nl/kringlooptilburg/productservice/config/PopulateDatabaseConfig.java
+++ src/main/java/nl/kringlooptilburg/productservice/config/PopulateDatabaseConfig.java
@@ -1,26 +1,26 @@
-package nl.kringlooptilburg.productservice.config;
-
-import nl.kringlooptilburg.productservice.domain.entities.ColorEntity;
-import nl.kringlooptilburg.productservice.domain.entities.enums.Color;
-import nl.kringlooptilburg.productservice.repositories.ColorRepository;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class PopulateDatabaseConfig {
-
-    // Populate the database with the colors, adding a new color is currently not possible
-    @Bean
-    public CommandLineRunner populateColorRepository(ColorRepository colorRepository) {
-        return args -> {
-            if (colorRepository.count() == 0) {
-                for (Color color : Color.values()) {
-                    ColorEntity colorEntity = new ColorEntity();
-                    colorEntity.setColor(color);
-                    colorRepository.save(colorEntity);
-                }
-            }
-        };
-    }
-}
+//package nl.kringlooptilburg.productservice.config;
+//
+//import nl.kringlooptilburg.productservice.domain.entities.ColorEntity;
+//import nl.kringlooptilburg.productservice.domain.entities.enums.Color;
+//import nl.kringlooptilburg.productservice.repositories.ColorRepository;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class PopulateDatabaseConfig {
+//
+//    // Populate the database with the colors, adding a new color is currently not possible
+//    @Bean
+//    public CommandLineRunner populateColorRepository(ColorRepository colorRepository) {
+//        return args -> {
+//            if (colorRepository.count() == 0) {
+//                for (Color color : Color.values()) {
+//                    ColorEntity colorEntity = new ColorEntity();
+//                    colorEntity.setColor(color);
+//                    colorRepository.save(colorEntity);
+//                }
+//            }
+//        };
+//    }
+//}
